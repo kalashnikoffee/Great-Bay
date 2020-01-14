e9f1c69dfc5b2bc01d68d23506ab8ff716a84bc5
Lucas Coffee <lsterlingcoffee@gmail.com>
2020-01-14 13:53:28 -0800

    seed.db

---
+++ seed_db.sql
@@ -0,0 +1,11 @@
+USE greatBay_db;
+
+INSERT INTO bidItems (item, bid)
+VALUES 
+("car", 2000),
+("computer" 500),
+("table", 50);
+
+INSERT INTO allBids (bidID, currentBid)
+VALUES (1, 5000), (1, 2500), (2, 700), (2, 250), (3, 30), (3, 70);
+
